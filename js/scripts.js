72f276841d5353cb27b389e2b5dcb7fbac56fa62
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2024-06-04 16:36:31 -0300

    Antes de mejorar algo 040624_16-35hs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    let menuIconBars = document.querySelector("#myTopnav .fa-bars");
+    let menuIconTimes = document.querySelector("#myTopnav .fa-times");
+
+    if (menuIconBars.className === "fa fa-bars") {
+        menuIconBars.className += " no-display";
+        menuIconTimes.className = "fa fa-times";
+    } else {
+        menuIconBars.className = "fa fa-bars";
+        menuIconTimes.className += " no-display";
+    }
+    
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {

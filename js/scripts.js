2387b194ab012b9155dcbec0bd53d16af11354e6
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2024-06-12 13:44:12 -0300

    Antes de mejorar algo 120624_1344hs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+// Función para hacer scroll suave hacia una sección
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Scroll to top button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    // document.body.scrollTop = 0; // For Safari
+    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    scrollToSection('top');
+}
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -11,7 +40,7 @@ function myFunction() {
         menuIconBars.className = "fa fa-bars";
         menuIconTimes.className += " no-display";
     }
-    
+
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {

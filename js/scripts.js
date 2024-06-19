a047f9351f11b628113a7f32640b36385982d55e
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2024-06-19 00:09:18 -0300

    Antes de mejorar algo 190624_0009hs

--- js/scripts.js
+++ js/scripts.js
@@ -48,15 +48,19 @@ function myFunction() {
     }
 }
 
-// Get the modal
+// Get the modals
 var modal = document.getElementById("myModal");
+let modalQR = document.querySelector('#qrModal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 let btn2 = document.getElementById('myBtn2');
+let btn3 = document.querySelector('#myBtn3');
+let btn4 = document.querySelector('#myBtn4');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+let span2 = document.querySelector('#qrModal .close');
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -65,17 +69,37 @@ btn.onclick = function () {
 
 btn2.addEventListener('click', () => {
     modal.style.display = 'block';
-})
+});
+
+btn3.addEventListener('click', () => {
+    myFunction();
+    modalQR.style.display = 'block';
+});
+
+btn4.addEventListener('click', () => {
+    myFunction();
+    modalQR.style.display = 'block';
+});
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
+span2.onclick = function () {
+    modalQR.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+window.onclick = function (event) {
+    if (event.target == modalQR) {
+        modalQR.style.display = "none";
     }
 }
 
